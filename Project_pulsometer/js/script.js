c65ee21a95b923d1e395ad5441f97e206ba1f98c
oyoha <vasal30000@mail.ru>
2023-07-13 22:35:52 +0800

    fixed sliders errors with icons(arrows) and scripts

--- Project_pulsometer/js/script.js
+++ Project_pulsometer/js/script.js
@@ -2,9 +2,9 @@ $(document).ready(function () {
   $(".carousel__inner").slick({
     speed: 600,
     prevArrow:
-      '<button type="button" class="slick-prev"><img src="../icons/previous-arrow.png" alt="previous_arrow"></button>',
+      '<button type="button" class="slick-prev"><img src="icons/previous-arrow.png" alt="previous_arrow"></button>',
     nextArrow:
-      '<button type="button" class="slick-next"><img src="../icons/next-arrow.png" alt="next_arrow"></button>',
+      '<button type="button" class="slick-next"><img src="icons/next-arrow.png" alt="next_arrow"></button>',
     responsive: [
       {
         breakpoint: 992,
