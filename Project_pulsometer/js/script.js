78f6d941c41cc5d5db78a8c01e5314c0e7d0f9c8
oyoha <vasal30000@mail.ru>
2023-07-13 22:28:32 +0800

    add slider

--- Project_pulsometer/js/script.js
+++ Project_pulsometer/js/script.js
@@ -1,1 +1,17 @@
-alert(1);
+$(document).ready(function () {
+  $(".carousel__inner").slick({
+    speed: 600,
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="../icons/previous-arrow.png" alt="previous_arrow"></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="../icons/next-arrow.png" alt="next_arrow"></button>',
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          arrows: false,
+        },
+      },
+    ],
+  });
+});
