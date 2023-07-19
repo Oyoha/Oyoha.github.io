7dac2f5525a5f46c4e5b70bfd19d65dc6b0685fb
oyoha <vasal30000@mail.ru>
2023-07-19 21:47:24 +0800

    add validate for my forms

--- Project_pulsometer/js/script.js
+++ Project_pulsometer/js/script.js
@@ -78,4 +78,29 @@ $(document).ready(function () {
       $(".overlay, #order").fadeIn("slow");
     });
   });
+
+  function validateForms(form) {
+    $(form).validate({
+      rules: {
+        name: "required",
+        phone: "required",
+        email: {
+          required: true,
+          email: true,
+        },
+      },
+      messages: {
+        name: "Пожалуйста введите своё имя",
+        phone: "Пожалуйста введите свой номер телефона",
+        email: {
+          required: "Пожалуйста введите свою почту",
+          email: "Ваша почта дожна быть формата name@domain.com",
+        },
+      },
+    });
+  }
+
+  validateForms("#consultation-form");
+  validateForms("#consultation form");
+  validateForms("#order form");
 });
